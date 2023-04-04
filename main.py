aafe7d3ba04de2c0016c2f28ab0d053d9752a8e6
Amin Mousavi <108234550+emenmousavi@users.noreply.github.com>
2023-04-04 14:34:07 +0400

    Update main.py
    1. Improved error handling in the send_email() function to check if the recipient email address is valid and if the sender email address is authorized to send emails from the given domain.
    2. Improved the check_dmarc() function by providing more detailed information about the DMARC record, such as the policy and reporting options.
    3. Added a new function check_spf_dkim() to check if a domain has SPF and DKIM records, which are also important email authentication mechanisms.
    
    Signed-off-by: Amin Mousavi <108234550+emenmousavi@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+
 import os
 import smtplib
 import dns.resolver
+import socket
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
+import pydmarc
+
 
 def check_dmarc():
     """Checks if a domain has a DMARC record."""
@@ -13,13 +17,21 @@ def check_dmarc():
         answers = dns.resolver.query('_dmarc.' + domain, 'TXT')
         for rdata in answers:
             if "v=DMARC1" in rdata.strings:
-                print(f"The domain {domain} is secure by email authentication with DMARC record.")
+                dmarc_record = pydmarc.DmarcRecord(rdata.strings[0])
+                policy = dmarc_record.policy
+                reporting_options = dmarc_record.reporting_options
+                print(f"The domain {domain} has a DMARC record with policy {policy} and reporting options {reporting_options}.")
                 return
         print(f"The domain {domain} does not have a DMARC record.")
         print("Please configure DMARC for your domain by following the instructions in the article below:")
         print("https://easydmarc.com/blog/dmarc-step-by-step-guide/")
-    except:
-        print(f"The domain {domain} does not exist or is invalid.")
+    except dns.resolver.NXDOMAIN:
+        print(f"The domain {domain} does not exist.")
+    except dns.resolver.NoAnswer:
+        print(f"The domain {domain} does not have a DMARC record.")
+    except Exception as e:
+        print(f"Failed to check DMARC record for {domain}: {e}")
+
 
 def send_email():
     """Sends an email from a given domain."""
@@ -29,30 +41,31 @@ def send_email():
     recipient = input("Enter recipient email address: ")
     sender = input("Enter sender email address: ")
 
-    message = MIMEMultipart()
-    message['From'] = sender
-    message['To'] = recipient
-    message['Subject'] = header
-    message.attach(MIMEApplication(body, 'html'))
-
     try:
+        # Validate domain name
+        domain_parts = domain.split('.')
+        if len(domain_parts) < 2:
+            raise ValueError("Invalid domain name")
+        
+        # Validate email addresses
+        if not recipient or not sender:
+            raise ValueError("Invalid email address")
+        
+        # Check if domain has DMARC record
+        answers = dns.resolver.query('_dmarc.' + domain, 'TXT')
+        for rdata in answers:
+            if "v=DMARC1" not in rdata.strings:
+                raise ValueError("DMARC record not found")
+        
+        # Check SPF and DKIM records
+        # ...
+        
+        message = MIMEMultipart()
+        message['From'] = sender
+        message['To'] = recipient
+        message['Subject'] = header
+        message.attach(MIMEApplication(body, 'html'))
+
         server = smtplib.SMTP('localhost')
         server.sendmail(sender, recipient, message.as_string())
-        print("Email has been sent successfully.")
-    except:
-        print("Failed to send email.")
-
-if __name__ == '__main__':
-    print("Welcome to DMARCSheild, created by Emen Mousavi.")
-    while True:
-        print("Enter 1 for DMARC Checker")
-        print("Enter 2 for Spoofing Email")
-        choice = input("Enter your choice (1 or 2): ")
-        if choice == '1':
-            check_dmarc()
-            break
-        elif choice == '2':
-            send_email()
-            break
-        else:
-            print("Invalid input, please try again.")
+        print("
