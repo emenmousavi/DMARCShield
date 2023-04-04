ae204eee9f5a51d87e0f14fe9acd2c27816b0be8
Amin Mousavi <108234550+emenmousavi@users.noreply.github.com>
2023-04-04 14:28:23 +0400

    Update main.py
    Signed-off-by: Amin Mousavi <108234550+emenmousavi@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
-
 import os
 import smtplib
 import dns.resolver
-import email.mime.text
+from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 
-def dmarc_checker():
+def check_dmarc():
+    """Checks if a domain has a DMARC record."""
     domain = input("Enter a domain to check for DMARC record: ")
     try:
         answers = dns.resolver.query('_dmarc.' + domain, 'TXT')
         for rdata in answers:
             if "v=DMARC1" in rdata.strings:
-                print("The domain {} is secure by email authentication with DMARC record.".format(domain))
+                print(f"The domain {domain} is secure by email authentication with DMARC record.")
                 return
-        print("The domain {} does not have a DMARC record.".format(domain))
+        print(f"The domain {domain} does not have a DMARC record.")
         print("Please configure DMARC for your domain by following the instructions in the article below:")
         print("https://easydmarc.com/blog/dmarc-step-by-step-guide/")
     except:
-        print("The domain {} does not exist or is invalid.".format(domain))
+        print(f"The domain {domain} does not exist or is invalid.")
 
 def send_email():
+    """Sends an email from a given domain."""
     domain = input("Enter your domain name: ")
     header = input("Enter email header: ")
     body = input("Enter email body: ")
@@ -48,7 +49,7 @@ if __name__ == '__main__':
         print("Enter 2 for Spoofing Email")
         choice = input("Enter your choice (1 or 2): ")
         if choice == '1':
-            dmarc_checker()
+            check_dmarc()
             break
         elif choice == '2':
             send_email()
